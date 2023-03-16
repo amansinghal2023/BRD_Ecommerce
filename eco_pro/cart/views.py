23ee337d1ea0d8d2f7a8366e773fcbd7687ffd87
muskanantino <muskan.k@antino.io>
2023-03-16 12:03:14 +0530

    comit before pull

--- eco_pro/cart/views.py
+++ eco_pro/cart/views.py
@@ -33,3 +33,27 @@ class GetCartItem(APIView):
         # product = list(Product.objects.filter(id = cart_obj.product[0]).values())
 
         return Response({"status":200,"error" : False, "product" : cart_obj})
+
+class CartItemDelete(APIView):
+    """
+    Delete a cart item.
+    """
+    def delete(self, request):
+        product_id = request.GET.get('id')
+        email= request.GET.get('email')
+
+        print("???????????????????????",product_id)
+
+        email_obj= Cart.objects.filter(user=email)
+
+        print("------------------------------------>",email_obj)
+        print(email_obj)
+        if len(email_obj)==0:
+            return Response({"status":400,"error" : True, "messasge":"Invalid User"})
+        else:
+            cart_obj= Cart.objects.filter(user=email_obj[0],product=[product_id])
+            # cart_obj= Cart.objects.get(user_id= email_obj[0])
+
+            print("----------------<<<<<<",cart_obj)
+            cart_obj.delete()
+            return Response({"status":200,"error" : False, "messasge":"Item has been Deleted From The cart"})
